1de66adfba17c5e72a867a20ef9ee4aab12fa6a0
Marek <rkx1901@gmail.com>
2014-10-30 12:19:53 -0500

    Add index for all memes templates, canvas is working

--- app/assets/javascripts/memes.js
+++ app/assets/javascripts/memes.js
@@ -1,13 +1,16 @@
 $(window).load(function(){
+  console.log("inside load function")
   function updateCanvas(){
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext("2d");
   var imageObj = new Image();
 
   function drawCanvas(){
+    console.log(imageObj)
     context.drawImage(imageObj, 0, 0);
   }
   function updateText(){
+    console.log("inside updatetext")
     context.font = "40pt Calibri";
       var upperText = $('textarea#upperText').val().toUpperCase();
       var bottomText = $('textarea#bottomText').val().toUpperCase();
@@ -15,8 +18,9 @@ $(window).load(function(){
       context.fillText(bottomText, 20, 540);
   }
 
-  imageObj.src = "test.jpg";
+  imageObj.src = $("#canvas_image").attr('src');
   imageObj.onload = function(){
+    console.log("inside onload")
     drawCanvas();
     updateText();
   }
--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,6 +1,14 @@
 class MemesController < ApplicationController
-  
+  def index
+    @templates =  Template.all
+  end
+
+  def new
+    @template = Template.find(params[:template_id])
+    @demot = Demot.new
+  end
+
   def demot_params
-   params.require(:demot).permit(:image)
+    params.require(:demot).permit(:image)
   end
 end
