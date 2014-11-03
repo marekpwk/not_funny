e8187fa224dca452928f3002c7cd36453c16b78d
Marek <rkx1901@gmail.com>
2014-11-03 14:27:57 -0600

    Add user action and view for edit

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -10,6 +10,15 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-   xit { should have_content('Sign up') }
+   it { should have_content('Sign up') }
+  end
+
+  describe "edit page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { expect(page).to have_selector('h2', text: "Update your profile") }
+    end 
   end
 end
