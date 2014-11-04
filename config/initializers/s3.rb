f2e92033f98d403f75f3baf60fd833790cca3b18
Marek <rkx1901@gmail.com>
2014-11-04 09:35:40 -0600

    Change carrierwave to store file locally in dev and test env

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider => 'AWS',
-    :aws_access_key_id => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    # :region
-  }
-  config.fog_directory = ENV['S3_BUCKET_NAME']
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET'],
+      # :region
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+  end
 end
