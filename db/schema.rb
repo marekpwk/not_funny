b87efb7eac74f6804c2978ec555ce80b031afa15
Marek <rkx1901@gmail.com>
2014-11-12 20:53:01 -0600

    Add column votes to demot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029224631) do
+ActiveRecord::Schema.define(version: 20141113024832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141029224631) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.integer  "votes"
   end
 
   create_table "templates", force: true do |t|
