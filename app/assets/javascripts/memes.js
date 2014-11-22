8058f7e6f503d2bbeece86b6e9c580482ab9adfa
Marek <rkx1901@gmail.com>
2014-11-22 15:34:26 -0600

    cors id workiiiiiiiing

--- app/assets/javascripts/memes.js
+++ app/assets/javascripts/memes.js
@@ -1,28 +1,29 @@
 $(window).load(function(){
   function updateCanvas(){
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext("2d");
-  var imageObj = new Image();
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+    var imageObj = new Image();
 
-  function drawCanvas(){
-    context.drawImage(imageObj, 0, 0);
-  }
-  function updateText(){
-    context.font = "40pt Calibri";
+    function drawCanvas(){
+      context.drawImage(imageObj, 0, 0);
+    }
+    function updateText(){
+      context.font = "40pt Calibri";
       var upperText = $('textarea#upperText').val().toUpperCase();
       var bottomText = $('textarea#bottomText').val().toUpperCase();
       context.fillText(upperText, 20, 80);
       context.fillText(bottomText, 20, 540);
       var url = canvas.toDataURL('image/jpeg');
-      $("#meme_image").val(url)    
-  }
-
-  imageObj.src = $("#canvas_image").html();
-  imageObj.onload = function(){
-    drawCanvas();
-    updateText();
-  }
+      $("#meme_image").val(url)
+    }
+    imageObj.crossOrigin = "Anonymous";
+    imageObj.src = $("#canvas_image").html();
+    imageObj.onload = function(){
+      drawCanvas();
+      updateText();
+    }
   };
+
   updateCanvas();
   $('textarea#upperText, textarea#bottomText').change(function(){updateCanvas()});
   $('textarea#upperText, textarea#bottomText').keyup(function(){updateCanvas()});
