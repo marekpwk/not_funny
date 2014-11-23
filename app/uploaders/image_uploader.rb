42b944ffe058fb15994c7bd2b079832123336df2
Marek <rkx1901@gmail.com>
2014-11-23 10:35:59 -0600

    Create unique names for uploaded images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -52,5 +52,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+ def filename
+   "#{model.user_id}user#{secure_token}.#{file.extension}" if original_filename.present?
+  end
 
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
