b54f258f8f876bd2c4dca5fdbcdedbbba932e1a5
Marek <rkx1901@gmail.com>
2014-12-22 18:38:05 -0600

    Remove notification controller

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,6 @@ Rails.application.routes.draw do
   post 'twilio/status' => 'twilio#status'
   post 'twilio/inbound' => 'twilio#inbound'
 
-  post 'notifications/notify' => 'notifications#notify'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
