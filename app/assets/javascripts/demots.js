1dd19cf36d2ac7ad35fe33a7fd69e56897fe3c44
Marek <rkx1901@gmail.com>
2014-12-23 23:29:00 -0600

    Hide pagination div

--- app/assets/javascripts/demots.js
+++ app/assets/javascripts/demots.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   if($('.pagination').length){
-    // $('.pagination').hide();
+    $('.pagination').hide();
     $(window).scroll(function(){
       var url = $('.pagination a[rel="next"]').attr('href');
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50){
