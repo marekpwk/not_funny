2cb9e883f898cce1db093c950eee32c7b7604acb
Marek <rkx1901@gmail.com>
2014-12-24 16:50:44 -0600

    set title

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -18,7 +18,7 @@ class TwilioController < ApplicationController
 
   def inbound
     user= User.find(ENV['MMS_USER'])
-    title = "Sent from cell"
+    title = params[:Body].empty? ? "Sent from cell" : params[:body]
     demot = Demot.new(title: title, user: user )
     demot.remote_image_url = params[:MediaUrl0]
     if demot.save
