96f1fd0863982f70076f2f729a42bb004b1c7915
Marek <rkx1901@gmail.com>
2014-12-25 14:19:44 -0600

    Fix css selector for loading scroll script

--- app/assets/javascripts/demots.js
+++ app/assets/javascripts/demots.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-  if($('#demots-wrapper .pagination').length){
+  if($('.demots-pag .pagination').length){
     $('.pagination').hide();
     $(window).scroll(function(){
       var url = $('.pagination a[rel="next"]').attr('href');
