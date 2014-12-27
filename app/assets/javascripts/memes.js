bd15414da5511ec7f7d5ec318ab48a19407c1f10
Marek <rkx1901@gmail.com>
2014-12-26 22:56:37 -0600

    Add check for canvas in memes.js

--- app/assets/javascripts/memes.js
+++ app/assets/javascripts/memes.js
@@ -1,31 +1,33 @@
-$(window).load(function(){
-  function updateCanvas(){
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext("2d");
-    var imageObj = new Image();
+$(document).ready(function(){
+ if($('#canvas').length){
+    function updateCanvas(){
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext("2d");
+      var imageObj = new Image();
 
-    function drawCanvas(){
-      canvas.height = imageObj.height;
-      context.drawImage(imageObj, 0, 0);
-    }
-    function updateText(){
-      context.font = "40pt Calibri";
-      var upperText = $('textarea#upperText').val().toUpperCase();
-      var bottomText = $('textarea#bottomText').val().toUpperCase();
-      context.fillText(upperText, 20, 80);
-      context.fillText(bottomText, 20, 540);
-      var url = canvas.toDataURL('image/jpeg');
-      $("#meme_image").val(url)
-    }
-    imageObj.crossOrigin = "Anonymous";
-    imageObj.src = $("#canvas_image").html();
-    imageObj.onload = function(){
-      drawCanvas();
-      updateText();
-    }
-  };
+      function drawCanvas(){
+        canvas.height = imageObj.height;
+        context.drawImage(imageObj, 0, 0);
+      }
+      function updateText(){
+        context.font = "40pt Calibri";
+        var upperText = $('textarea#upperText').val().toUpperCase();
+        var bottomText = $('textarea#bottomText').val().toUpperCase();
+        context.fillText(upperText, 20, 80);
+        context.fillText(bottomText, 20, 540);
+        var url = canvas.toDataURL('image/jpeg');
+        $("#meme_image").val(url)
+      }
+      imageObj.crossOrigin = "Anonymous";
+      imageObj.src = $("#canvas_image").html();
+      imageObj.onload = function(){
+        drawCanvas();
+        updateText();
+      }
+    };
 
-  updateCanvas();
-  $('textarea#upperText, textarea#bottomText').change(function(){updateCanvas()});
-  $('textarea#upperText, textarea#bottomText').keyup(function(){updateCanvas()});
+    updateCanvas();
+    $('textarea#upperText, textarea#bottomText').change(function(){updateCanvas()});
+    $('textarea#upperText, textarea#bottomText').keyup(function(){updateCanvas()});
+ }
 });
