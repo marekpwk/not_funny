7822171f3d4ef30a2ea6be4e40cc6f58d3f34dae
Marek <rkx1901@gmail.com>
2014-12-29 09:26:23 -0600

    Add validations to Template

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,9 @@
 class Template < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :user
-
+  validates :image, presence: true
+  validates :user, presence: true
+  validates :title, presence: true
   def self.search(query)
     if query
       where('title ILIKE  ?', "%#{query.downcase}%")
