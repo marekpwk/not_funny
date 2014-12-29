a361f27331152c6b46d9230af6cb890941e59acf
Marek <rkx1901@gmail.com>
2014-12-28 21:32:51 -0600

    Update selectors in js files, so they work with tables

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -33,9 +33,9 @@ class Admin::UsersController < ApplicationController
 
   def edit
     @user = User.find_by(id: params[:id])
-    # respond_to do |format|
-    #   format.js
-    # end
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
