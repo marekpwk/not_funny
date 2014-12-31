1f788c096f3284353fe9eb1dfd81a9f99756e266
Marek <rkx1901@gmail.com>
2014-12-30 19:34:58 -0600

    Add approved column for demots

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226195427) do
+ActiveRecord::Schema.define(version: 20141231012814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141226195427) do
     t.datetime "updated_at"
     t.string   "type"
     t.integer  "total_votes", default: 0
+    t.boolean  "approved",    default: false
   end
 
   create_table "templates", force: true do |t|
