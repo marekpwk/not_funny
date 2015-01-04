4378e5fdc5ae2f2940fddeb1aa19ed393ac11320
Marek <rkx1901@gmail.com>
2015-01-04 16:49:38 -0600

    Create polymorphic Comment model and migration

--- app/models/demot.rb
+++ app/models/demot.rb
@@ -1,5 +1,6 @@
 class Demot < ActiveRecord::Base
   belongs_to :user, foreign_key: "user_id"
+  has_many :comments, as: :commentable
   mount_uploader :image, ImageUploader
   validates :image, presence: true
   validates :user, presence: true
