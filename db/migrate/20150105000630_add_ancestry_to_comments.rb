7e6c018616d5841a0a9906fe4b0837e8d64ad1d9
Marek <rkx1901@gmail.com>
2015-01-04 18:25:38 -0600

    Fix add_index

--- db/migrate/20150105000630_add_ancestry_to_comments.rb
+++ db/migrate/20150105000630_add_ancestry_to_comments.rb
@@ -1,6 +1,6 @@
 class AddAncestryToComments < ActiveRecord::Migration
   def change
     add_column :comments, :ancestry, :string
+    add_index :comments, :ancestry
   end
-  add_index :comments, :ancestry
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104222426) do
+ActiveRecord::Schema.define(version: 20150105000630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20150104222426) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ancestry"
   end
+
+  add_index "comments", ["ancestry"], name: "index_comments_on_ancestry", using: :btree
 
   create_table "demots", force: true do |t|
     t.string   "title"
