4aa7f95777cb751bd472c249278e76b5ed5f6b02
Marek <rkx1901@gmail.com>
2015-01-05 19:54:30 -0600

    Create Comment model and add associations to user and demot

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   has_ancestry
+  belongs_to :demot
+  belongs_to :commentable, :polymorphic => true
+  belongs_to :user
 end
--- app/models/demot.rb
+++ app/models/demot.rb
@@ -1,6 +1,7 @@
 class Demot < ActiveRecord::Base
   belongs_to :user, foreign_key: "user_id"
-  has_many :comments, as: :commentable, :dependent => :destroy
+  has_many :comments, as: :commentable, :dependent => :destroy 
+  belongs_to :commentable, :polymorphic => true
   mount_uploader :image, ImageUploader
   validates :image, presence: true
   validates :user, presence: true
